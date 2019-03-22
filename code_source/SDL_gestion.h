b40164bede367b6de67fe2597753e08279d8b6b0
taher <taher.zribi@esprit.tn>
2019-03-22 19:38:05 +0100

    added health

---
+++ code_source/SDL_gestion.h
@@ -0,0 +1,18 @@
+#ifndef GESTION_H_INCLUDED
+#define GESTION_H_INCLUDED
+
+#include <SDL/SDL.h>
+#include "SDL_collision.h"
+#include "SDL_char.h"
+
+struct health{
+	int vie;
+	SDL_Rect position;
+	SDL_Surface *font_vie;
+};
+
+typedef struct health health;
+
+health gestionVie(hero player, hero villain, health vie);
+
+#endif
