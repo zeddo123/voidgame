fd2fae69545e8015d3d3594f757586ede4298676
kaissounetahfoun <47786086+kaissounetahfoun@users.noreply.github.com>
2019-03-23 16:54:47 +0100

    box-cercle/2 cercles

--- code_source/SDL_collision.c
+++ code_source/SDL_collision.c
@@ -29,3 +29,65 @@ int collisionBox(SDL_Rect a, SDL_Rect b){
 
 	return 1;
 }
+
+double distanceSquared( int x1, int y1, int x2, int y2 )
+{
+    int deltaX = x2 - x1;
+    int deltaY = y2 - y1;
+    return deltaX*deltaX + deltaY*deltaY;
+}
+
+int collisionCxC( Circle a, Circle b )
+{
+    int totalRadiusSquared = a.r + b.r;
+    totalRadiusSquared = totalRadiusSquared * totalRadiusSquared;
+    if( distanceSquared( a.x, a.y, b.x, b.y ) < ( totalRadiusSquared ) )
+    {
+        return 1;
+    }
+    return 0;
+}
+
+
+
+int collisionBxC( Circle& a, SDL_Rect& b )
+{
+    
+    int centreX, centreY;
+
+    
+    if( a.x < b.x )
+    {
+        cX = b.x;
+    }
+    else if( a.x > b.x + b.w )
+    {
+        cX = b.x + b.w;
+    }
+    else
+    {
+        cX = a.x;
+    }
+ if( a.y < b.y )
+    {
+        cY = b.y;
+    }
+    else if( a.y > b.y + b.h )
+    {
+        cY = b.y + b.h;
+    }
+    else
+    {
+        cY = a.y;
+    }
+
+
+    if( distanceSquared( a.x, a.y, cX, cY ) < a.r * a.r )
+    {
+        //This box and the circle have collided
+        return 1;
+    }
+
+
+    return 0;
+}
