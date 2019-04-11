16789cf2bc81f182149ed1aa2264bc6852133da3
kaissounetahfoun <47786086+kaissounetahfoun@users.noreply.github.com>
2019-04-11 14:10:41 +0200

    updated colli CxC/BxC

--- code_source/SDL_collision.h
+++ code_source/SDL_collision.h
@@ -9,11 +9,14 @@ typedef struct{
     int r;
 }Circle;
 
-double distanceSquared(int x1, int y1, int x2, int y2);
+/*double distanceSquared(int x1, int y1, int x2, int y2);
 
 int collisionBxC(Circle a, SDL_Rect b);
 
-int collisionCxC(Circle a, Circle b);
+int collisionCxC(Circle a, Circle b);*/
+bool CollisionTrigoCir ( SDL_Surface *Player , SDL_Surface *C_Object , SDL_Rect Pposition , SDL_Rect Oposition ) ; 
+ 
+bool CollisionTrigoInsc ( SDL_Surface *Player , SDL_Surface *C_Object , SDL_Rect position , SDL_Rect position1  ) ; 
 
 int collisionBox(SDL_Rect a, SDL_Rect b);
 
