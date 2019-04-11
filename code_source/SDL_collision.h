4275a879afaaccd7ec8829ee5296876d20b643bb
kaissounetahfoun <47786086+kaissounetahfoun@users.noreply.github.com>
2019-04-11 14:21:10 +0200

    Update SDL_collision.h

--- code_source/SDL_collision.h
+++ code_source/SDL_collision.h
@@ -14,9 +14,9 @@ typedef struct{
 int collisionBxC(Circle a, SDL_Rect b);
 
 int collisionCxC(Circle a, Circle b);*/
-bool CollisionTrigoCir ( SDL_Surface *Player , SDL_Surface *C_Object , SDL_Rect Pposition , SDL_Rect Oposition ) ; 
+int CollisionTrigoCir ( SDL_Surface *Player , SDL_Surface *C_Object , SDL_Rect Pposition , SDL_Rect Oposition ) ; 
  
-bool CollisionTrigoInsc ( SDL_Surface *Player , SDL_Surface *C_Object , SDL_Rect position , SDL_Rect position1  ) ; 
+int CollisionTrigoInsc ( SDL_Surface *Player , SDL_Surface *C_Object , SDL_Rect position , SDL_Rect position1  ) ; 
 
 int collisionBox(SDL_Rect a, SDL_Rect b);
 
