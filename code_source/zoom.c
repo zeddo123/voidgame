985b3900d5c297653ecc2a37a3a2b6f79eb87e27
zeddo123 <mouhadrissi9@gmail.com>
2019-05-05 17:25:44 +0200

    merge

--- code_source/zoom.c
+++ code_source/zoom.c
@@ -3,21 +3,18 @@
 #include <SDL/SDL_image.h>
 #include <SDL/SDL_rotozoom.h>
 #include "zoom.h"
-void roto(SDL_Surface *screen,SDL_Surface *image, SDL_Surface *rotation ,SDL_Rect rect , SDL_Event event)
-{
+void roto(SDL_Surface *screen,SDL_Surface *image, SDL_Surface *rotation ,SDL_Rect rect , SDL_Event event){
 	int tempsActuel = 0; int tempsPrecedent = 0; int done;
 	done = 1; int angle = 0;
-	while(done)
-    	{
-        	angle += 2;
+	while(done){
+		angle += 2;
+		SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 255, 255, 255));
  
- 
-       SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 255, 255, 255));
- 
-        rotation = rotozoomSurface(image, angle, 1.5, 1);
-        SDL_BlitSurface(rotation , NULL, screen, &rect); 
-	SDL_Flip(screen);        
-	SDL_FreeSurface(rotation); 
+		rotation = rotozoomSurface(image, angle, 1.5, 1);
+		SDL_BlitSurface(rotation , NULL, screen, &rect); 
+		
+		SDL_Flip(screen);        
+		SDL_FreeSurface(rotation); 
     }
 }
 
