23e8c10ff4e4940f0fbf5fb76dce6e290eead733
rayenkkj <32436999+rayenkkj@users.noreply.github.com>
2019-05-05 18:39:14 +0100

    acceleration modiffications
    idk where or when to place in main pool

---
+++ acceleration/SDL_acceleration.h
@@ -0,0 +1,13 @@
+#ifndef ACCELERATION_H_INCLUDED
+#define ACCELERATION_H_INCLUDED
+
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include "SDL_char.h"
+
+
+void Acceleration(hero *h);
+void Decceleration(hero *h);
+
+#endif
