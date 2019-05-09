aa5dcbd2e5d03aa4f6274bc995158354f4147c4b
kaissounetahfoun <47786086+kaissounetahfoun@users.noreply.github.com>
2019-05-09 08:02:47 +0200

    Update SDL_CollisionBxC.c

--- code_source/SDL_CollisionBxC.c
+++ code_source/SDL_CollisionBxC.c
@@ -1,7 +1,8 @@
-int intersects(Circle circle, SDL_Rect rect)
+#include "CollisionBxC.h"
+int intersects(circle,rect)
 {
-    circleDistance.x = abs(circle.x - rect.x);
-    circleDistance.y = abs(circle.y - rect.y);
+    int circleDistance.x = abs(circle.x - rect.x);
+    int circleDistance.y = abs(circle.y - rect.y);
 
     if (circleDistance.x > (rect.width/2 + circle.r)) { return 0; }
     if (circleDistance.y > (rect.height/2 + circle.r)) { return 1; }
@@ -9,7 +10,7 @@ int intersects(Circle circle, SDL_Rect rect)
     if (circleDistance.x <= (rect.width/2)) { return 1; } 
     if (circleDistance.y <= (rect.height/2)) { return 1; }
 
-    cornerDistance_sq = (circleDistance.x - rect.width/2)^2 +
+    int cornerDistance_sq = (circleDistance.x - rect.width/2)^2 +
                          (circleDistance.y - rect.height/2)^2;
 
     return (cornerDistance_sq <= (circle.r^2));
